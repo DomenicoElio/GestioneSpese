b90b75cdcc0f802fa43e55e98b977ece1f993471
Domenico Elio Bressanello <domenicoelio.bressanello@gmail.com>
2022-05-06 15:25:42 +0200

    minor change

--- Entities/Expenses.cs
+++ Entities/Expenses.cs
@@ -6,21 +6,24 @@ using System.Threading.Tasks;
 
 namespace GestioneSpese.Entities
 {
+    public enum Cat
+    {
+        Viaggio,
+        Alloggio,
+        Vitto,
+        Altro
+    }
     public class Expenses
     {
-
         public DateTime Date { get; set; }
-
-        public string Category { get; set; }
-
+        public Cat Category { get; set; }
         public string Description { get; set; }
-        
-        public double Costs { get; set; }
+        public double Amount { get; set; }
 
-        public virtual void Print()
-        {
-            Console.WriteLine("Generica figura geometrica");
-        }
 
+        public virtual string ToRow() {return $"{Date.ToShortDateString};{Category};{Description};{Amount}";}
+
+        public override string ToString()
+        {return $"this is when he spent: {Date.ToShortDateString}" +$"this is the type of expense: {Category}" +$"this is what he did: {Description}" + $"The cost was: €{Amount}\n";}
     }
 }
